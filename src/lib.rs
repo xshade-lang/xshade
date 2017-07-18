d234eca7a871d2ea9811f6c55728dc233529493d
Andreas Fischer <home@deviru.de>
2017-07-18 19:51:47 +0200

    string builder and glsl backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate nom;
 
+pub mod glsl;
+
 pub mod ast;
 pub mod parser;
+pub mod string_builder;
 
 pub use ::ast::*;
 pub use parser::parse_str;
---
+++ src/string_builder.rs
@@ -0,0 +1,35 @@
+pub struct StringBuilder {
+    buffer: Vec<u8>,
+}
+
+impl StringBuilder {
+    pub fn new(capacity: usize) -> StringBuilder {
+        StringBuilder {
+            buffer: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn append(&mut self, s: &str) {
+        self.buffer.extend(s.as_bytes().iter().cloned());
+    }
+
+    pub fn to_string(self) -> Option<String> {
+        match String::from_utf8(self.buffer) {
+            Ok(string) => Some(string),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_string() {
+        let mut sb = StringBuilder::new(64);
+        sb.append("foo");
+        sb.append("bar");
+        assert_eq!(Some("foobar".to_string()), sb.to_string());
+    }
+}
